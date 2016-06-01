b2e8dbee515a654a95b7ba227d3a397db328708e
Dustin Lim <lim.dustin@gmail.com>
2016-06-01 12:51:26 +0200

    Make character movement independent of animation() call rate.

--- Source/Character.h
+++ Source/Character.h
@@ -9,11 +9,11 @@ public:
 
 	Vec3Df position = Vec3Df(0, 0, 0);
 	Vec3Df movementDirection = Vec3Df(0, 0, 0);
-	float movementSpeed = 0.002;
+	float movementSpeed = 1; //units per second
 	float size = 0.5;
 	Vec3Df color = Vec3Df(1, 1, 1);
 
 	void draw();
-	void animate();
+	void animate(int deltaTime);
 };
 
